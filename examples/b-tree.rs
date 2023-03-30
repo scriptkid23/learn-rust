b722aa260d8b079bdca641f307c9fd0ef4326b21
hoando <hoando.dev@gmail.com>
2023-03-30 17:07:05 +0700

    feat: create linked list

--- examples/b-tree.rs
+++ examples/b-tree.rs
@@ -1,3 +1,32 @@
+use std::rc::Rc;
+
+#[derive(Debug)]
+struct LinkedList {
+    head: Option<Box<Node>>,
+}
+#[derive(Debug, Clone)]
 struct Node {
-    
+    value: i32,
+    next: Option<Box<Node>>,
+}
+
+impl LinkedList {
+    fn new() -> Self {
+        LinkedList { head: None }
+    }
+    fn append(&self, value: i32) -> Self {
+        LinkedList {
+            head: Some(Box::new(Node {
+                value: value,
+                next: self.head.clone(), // clone not borrow
+            })),
+        }
+    }
+}
+
+fn main() {
+    // let x = Node::new(1);
+    let x = LinkedList::new().append(1).append(2).append(3);
+    print!("{:?}", x);
+    // print!("{}",x.get_value());
 }
