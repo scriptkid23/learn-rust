d256f8c388863c84ef32775ebec166d06c0b091c
hoando <hoando.dev@gmail.com>
2023-04-01 16:05:29 +0700

    refactor: update rust

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,20 @@ fn change(num: &mut i32) {
     *num = 3;
 }
 
+struct Character {
+    name: String,
+}
+
+impl Drop for Character {
+    fn drop(&mut self) {
+        print!("{} was dropped", self.name);
+    }
+}
 fn main() {
-    let mut num = 10;
-    change(&mut num);
-    println!("{:?}", num);
+    let steve = Character {
+        name: String::from("123"),
+    };
+
+
+    
 }
